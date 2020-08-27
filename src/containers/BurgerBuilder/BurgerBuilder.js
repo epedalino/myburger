9048fcb31d7a60bd90aa101422cef128db37905a
epedalino <epedalino@gmail.com>
2020-08-27 13:51:58 -0400

    Implement backdrop component that displays anytime the modal pricing summary screen displays. Upon clicking the backdrop the user can edit the order until it the ingredients are as desired

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,6 +36,7 @@ class BurgerBuilder extends Component {
       }, 0);
     this.setState({purchaseable: sum > 0})
   }
+
   addIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
     const updatedCount = oldCount + 1;
@@ -66,9 +67,15 @@ class BurgerBuilder extends Component {
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     this.updatePurchaseState(updatedIngredients);
   }
+
   purchaseHandler = () => {
     this.setState({purchasing: true});
   }
+
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  }
+
   render () {
     const disabledInfo = {
       ...this.state.ingredients
@@ -78,7 +85,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients} />
