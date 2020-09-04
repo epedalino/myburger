cb56e83668c9e70581a3d9b4c10fc955a030f782
epedalino <epedalino@gmail.com>
2020-09-03 23:42:02 -0400

    Add a Spinner to the Modal window that will display after the user clicks on Continue. It will indicate to the user that their hamburger order is being placed

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import clases from './Spinner.module.css';
+import classes from './Spinner.module.css';
+
+const spinner = () => (
+  <div className={classes.Loader}>Loading...</div>
+);
+
+export default spinner;
