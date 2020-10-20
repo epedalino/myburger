46db726728a40d7347d9f06373ff921ed9381fc1
epedalino <epedalino@gmail.com>
2020-10-20 10:57:54 -0400

    Update package for react-router-dom. Review css files for the burger and toolbar modules

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/BurgerBuilder/Checkout/Checkout';
@@ -7,10 +9,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Layout>
-          <BurgerBuilder />
-          <Checkout />
-
+         <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
