80b11e05147e3202ef3bb034891af41aa637358e
epedalino <epedalino@gmail.com>
2020-10-29 19:46:21 -0400

    Add spinner to contactData to display when a user orders the burger

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from '../ContactData/ContactData.module.css';
 import axios from '../../../axios-orders';
 
@@ -44,16 +45,22 @@ class ContactData extends Component {
   }
 
   render () {
-    return (
-      <div className={classes.ContactData}>
-        <h4>Enter your Contact Data</h4>
-        <form>
+    let form = (
+      <form>
           <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
           <input className={classes.Input} type="email" name="email" placeholder="Your email" />
           <input className={classes.Input} type="text" name="street" placeholder="Street" />
           <input className={classes.Input} type="text" name="zip" placeholder="Zip Code" />
           <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-        </form>
+      </form>
+    );
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
+    return (
+      <div className={classes.ContactData}>
+        <h4>Enter your Contact Data</h4>
+        {form}
       </div>
     );
   }
