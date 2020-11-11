564b58837099474bda768f2cbc7c99a97b306c56
epedalino <epedalino@gmail.com>
2020-11-11 14:17:24 -0500

    Handle user input in the order form by creating a change handler

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -10,18 +10,21 @@ const input = (props) => {
       inputElement = <input
         className={classes.InputElement}
         {...props.elementConfig}
-        value={props.value}/>;
-      break;
+        value={props.value}
+        onChange={props.changed}/>;
+       break;
     case ('textarea'):
       inputElement = <textarea
         className={classes.InputElement}
         {...props.elementConfig}
-        value={props.value}/>;
+        value={props.value}
+        onChange={props.changed}/>;
     case ('select'):
       inputElement = (
         <select
           className={classes.InputElement}
-          value={props.value}>
+          value={props.value}
+          onChange={props.changed}>
           {props.elementConfig.options.map(option => (
             <option key={option.value} value={option.value}>
               {option.displayValue}
